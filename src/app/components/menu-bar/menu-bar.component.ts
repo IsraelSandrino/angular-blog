f17aa748c6f96ec2fc6df610ea3639e93b64cb65
israelSandrino <isandrino99@gmail.com>
2023-09-26 22:27:37 -0300

    Adjusted navbar and add icons

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css','./menu-bar-responsive.component.css']
+  styleUrls: ['./menu-bar.component.css']
 })
-export class MenuBarComponent {
+export class MenuBarComponent implements OnInit {
 
+  photoLogo:string = "https://raelsandrini.com/wp-content/uploads/2023/07/nav-logo.png"
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faEnvelope = faEnvelope;
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+  }
 }
